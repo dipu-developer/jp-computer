4b4292edbdf54374a2e56e5dd5bf329a70ed5afe
Deepak Sharma <75806982+dipu-developer@users.noreply.github.com>
2023-03-26 12:20:24 +0530

    final project done

--- js/script.js
+++ js/script.js
@@ -1,36 +1,75 @@
-// Nav Bar Code 
+// Nav Bar Code
 var slides = document.querySelectorAll(".slide");
 var dots = document.querySelectorAll(".dot");
 var index = 0;
 
-
-function prevSlide(n){
-  index+=n;
+function prevSlide(n) {
+  index += n;
   console.log("prevSlide is called");
   changeSlide();
 }
 
-function nextSlide(n){
-  index+=n;
+function nextSlide(n) {
+  index += n;
   changeSlide();
 }
 
 changeSlide();
 
-function changeSlide(){
-  if(index>slides.length-1)
-    index=0;
-  if(index<0)
-    index=slides.length-1;
-    for(let i=0;i<slides.length;i++){
-      slides[i].style.display = "none";
-      dots[i].classList.remove("active"); 
-    }
-    slides[index].style.display = "block";
-    dots[index].classList.add("active");
-
+function changeSlide() {
+  if (index > slides.length - 1) index = 0;
+  if (index < 0) index = slides.length - 1;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active");
+  }
+  slides[index].style.display = "block";
+  dots[index].classList.add("active");
 }
 
+//========================== Typing ===========================
+const words = [
+  "Hii! I am John ",
+  "We provide the best facality service ",
+];
+let i = 0;
+let timer;
+
+function typingEffect() {
+  let word = words[i].split("");
+  var loopTyping = function () {
+    if (word.length > 0) {
+      document.getElementById("word").innerHTML += word.shift();
+    } else {
+      deletingEffect();
+      return false;
+    }
+    timer = setTimeout(loopTyping, 100);
+  };
+  loopTyping();
+}
+
+function deletingEffect() {
+  let word = words[i].split("");
+  var loopDeleting = function () {
+    if (word.length > 0) {
+      word.pop();
+      document.getElementById("word").innerHTML = word.join("");
+    } else {
+      if (words.length > i + 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      typingEffect();
+      return false;
+    }
+    timer = setTimeout(loopDeleting, 200);
+  };
+  loopDeleting();
+}
+
+typingEffect();
 
 
 
@@ -40,12 +79,4 @@ function changeSlide(){
 
 
 
-
-
-
-
-// ===================================================
-
-//========================== Swiper Slider  ===========================
-  
 
